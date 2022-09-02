56d25d6dfd51c9642c7fad388e034189db2e45f6
vitalik061981 <vitalik061981@mail.ru>
2022-09-02 12:00:39 +0300

    add Hometask14_1.py

---
+++ hometask14_1.py
@@ -0,0 +1,31 @@
+# hometask_14_1
+# Создать класс Dog.
+# Класс имеет четыре
+# атрибута: height, weight, name, age. Класс
+# имеет три метода: jump, run, bark. Каждый
+# метод выводит сообщение на экран.
+# Создать объект класса Dog, вызвать все
+# методы объекта и вывести на экран все его
+# атрибуты.
+
+
+class Dog:
+    def __init__(self,height, weight, name, age):
+        self.height = height
+        self.weight = weight
+        self.name = name
+        self.age = age
+
+    def jump(self, jump):
+        print(f'Собачка прыгает на {jump} метров')
+
+    def run(self, run):
+        print(f'Собачка бегает со скоростью {run} км/ч')
+
+    def bark(self, bark):
+        print(f'Собачка лает с громкостью {bark} дБ')
+
+shepherd_dog = Dog(1,8,'xcvb',4)
+
+print(shepherd_dog.jump(1),shepherd_dog.run(1),shepherd_dog.bark(1))
+print(shepherd_dog.height,shepherd_dog.weight,shepherd_dog.name,shepherd_dog.age)
