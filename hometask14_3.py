42b694c46a793571a2ca8c0b1bff5f3997258f9b
vitalik061981 <vitalik061981@mail.ru>
2022-09-13 13:30:56 +0300

    add Hometask14_3.py

--- hometask14_3.py
+++ hometask14_3.py
@@ -20,3 +20,22 @@ b = nname.capitalize()
 
 print(name1(b))
 print(' '.join(name1(b)))
+
+# Hometask_14_8
+# Добавьте в класс Pet дескриптор,
+# чтобы нельзя было задать
+# отрицательный возраст или 0
+
+class Pet:
+    def __init__(self,name, age, master):
+        self.name = name
+        self.__age = age
+        self.master = master
+
+    def set_age(self,age):
+        if age <= 0:
+            raise ValueError('Неправильный возраст')
+        self.__age=age
+
+dogg = Pet(10,-20,10)
+print(dogg.set_age(20))
