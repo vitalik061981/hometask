5d6f4d7f8269aec24750e05723c98dd2ff266a69
vitalik061981 <vitalik061981@mail.ru>
2022-09-13 14:28:40 +0300

    add Hometask14_3.py

--- hometask14_3.py
+++ hometask14_3.py
@@ -21,3 +21,29 @@ b = nname.capitalize()
 print(name1(b))
 print(' '.join(name1(b)))
 
+# Hometask_14_8
+# Добавьте в класс Pet дескриптор,
+# чтобы нельзя было задать
+# отрицательный возраст или 0
+
+class NonNegativeValue:
+    def __set_name__(self, owner, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if value <= 0:
+            raise ValueError(f"{self.name} cannot be negative")
+        instance.__dict__[self.name] = value
+
+class Pet:
+    age = NonNegativeValue()
+    def __init__(self, name, age, master):
+        self.name = name
+        self.age = age
+        self.master = master
+
+a = Pet("apples", -2.15, 2)
+print(a)
