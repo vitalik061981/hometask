13fcc673d368260918734a781408db7ab6ae9153
vitalik061981 <vitalik061981@mail.ru>
2022-09-13 14:30:35 +0300

    add Hometask14_3.py

--- hometask14_3.py
+++ hometask14_3.py
@@ -47,3 +47,27 @@ class Pet:
 
 a = Pet("apples", -2.15, 2)
 print(a)
+
+# Hometask_14_9
+# Добавьте в класс Pet валидацию,
+# чтобы у питомца было имя и хозяин
+
+class Pet:
+    def __init__(self,name, age, master):
+        self.__name = name
+        self.age = age
+        self.__master = master
+
+    def set_name(self,name):
+        if name <= 0:
+            raise ValueError('Нет имени')
+        self.__name=name
+
+    def set_master(self,master):
+        if master <= 0:
+            raise ValueError('Нет хозяина')
+        self.__master=master
+
+dogg = Pet(10,-20,10)
+print(dogg.set_name(-20))
+print(dogg.set_master(-20))
